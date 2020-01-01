4f84a162b3e848bf26ffd4b376585a60ea0b6d76
Kohei <andokohei@gmail.com>
2020-01-01 23:57:21 +0900

    Add git user setting script

---
+++ scripts/set_git_user.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# requires jq installed
+
+CONFIG_PATH=$HOME/util/config/git_users.json
+
+# check file existence
+if ! [ -f $CONFIG_PATH ]
+then
+  echo "Git users config file does not exist at a path => $CONFIG_PATH"
+  exit 1
+fi
+
+# argument check
+if [ -z $1 ]
+then
+  echo "Please specify the username."
+  exit 1
+fi
+
+USER_NAME=$1
+CONFIG_JSON=$(cat $CONFIG_PATH | jq ".$USER_NAME" -e)
+
+# data check
+if ! [ $? = 0 ]
+then
+  echo "Username $USER_NAME does not exist on config."
+  exit 1
+fi
+
+# properties check
+GIT_USER_NAME=$(cat $CONFIG_PATH | jq ".$USER_NAME.name" -re)
+if ! [ $? = 0 ]
+then
+  echo "'name' property is missing on config."
+  exit 1
+fi
+
+GIT_USER_EMAIL=$(cat $CONFIG_PATH | jq ".$USER_NAME.email" -re)
+if ! [ $? = 0 ]
+then
+  echo "'email' property is missing on config."
+  exit 1
+fi
+
+# actually set the git config here
+git config user.name $GIT_USER_NAME
+git config user.email $GIT_USER_EMAIL
+
+echo "Successfully set git user with 'name' $GIT_USER_NAME, 'email' $GIT_USER_EMAIL"
+echo "\ngit config"
+cat .git/config
